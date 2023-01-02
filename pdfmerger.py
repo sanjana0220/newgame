619f30ec79ecc7a1e231c59c88eb4f5b482986d2
Sanjana vishwaroop <68190849+sanjana0220@users.noreply.github.com>
2023-01-02 17:44:14 +0530

    Delete pdfmerger.py

--- pdfmerger.py
+++
@@ -1,8 +0,0 @@
-from PyPDF2 import PdfMerger
-pdfs=["1st sem.pdf","2nd sem.pdf","3rd sem.pdf"]
-merger = PdfMerger()
-
-for pdf in pdfs:
-  merger.append(pdf)
-merger.write("merged.pdf")
-merger.close()
